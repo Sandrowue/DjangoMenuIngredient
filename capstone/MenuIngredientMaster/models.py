25a30628ffc36d2e444d3fd9625a08e76e51363b
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-12-21 16:50:23 +0200

    Migration of four tables

--- capstone/MenuIngredientMaster/models.py
+++ capstone/MenuIngredientMaster/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=75)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=15)
+    unit_price = models.FloatField(default=0)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=75)
+    price = models.FloatField(default=0)
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField("date purchased")
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
