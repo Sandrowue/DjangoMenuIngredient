fffe0bf2429340e33cd8854ab20014c528c88694
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-12-30 00:28:27 +0200

    views in primitive form

--- capstone/MenuIngredientMaster/views.py
+++ capstone/MenuIngredientMaster/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to our kitchen")
+    return HttpResponse("Welcome to Django Delight's kitchen!")
+
+def ingredients(request):
+    return HttpResponse("All ingredients in the inventory:")
+
+def deleteIngredient(request, deleteIngredient=None):
+    return HttpResponse("You're about to delete the ingredient %s." % deleteIngredient)
+
+def aviableMenus(request):
+    return HttpResponse("The menus and drinks we serve:")
+
+def itemsInMenu(request, menu_item=None):
+    response = "This menu has following ingredients: %s."
+    return HttpResponse(response % menu_item)
+
+def financials(request):
+    return HttpResponse("Django Delight's profit and revenue.")
+
+def sold(request):
+    return HttpResponse("Recently sold Menus:")
