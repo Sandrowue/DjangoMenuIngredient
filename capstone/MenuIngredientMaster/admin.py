d8ed9099bf33438a620bed9454ef66a9cb9aaed7
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2024-12-30 00:28:04 +0200

    admit model registration

--- capstone/MenuIngredientMaster/admin.py
+++ capstone/MenuIngredientMaster/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
+
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(Purchase)
+admin.site.register(RecipeRequirement)
