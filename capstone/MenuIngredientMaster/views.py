21ab6a82b0d94d6a937067177eb493db681aa69b
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2025-01-02 23:23:40 +0200

    views with templates

--- capstone/MenuIngredientMaster/views.py
+++ capstone/MenuIngredientMaster/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Ingredient, MenuItem
 
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to Django Delight's kitchen!")
 
 def ingredients(request):
-    return HttpResponse("All ingredients in the inventory:")
+    allIngredients = Ingredient.objects.order_by("name")
+    template = loader.get_template("MenuIngredientMaster/ingredients.html")
+    context = {
+        "allIngredients": allIngredients,
+    }
+    return HttpResponse(template.render(context, request))
 
 def deleteIngredient(request, deleteIngredient=None):
     return HttpResponse("You're about to delete the ingredient %s." % deleteIngredient)
 
 def aviableMenus(request):
-    return HttpResponse("The menus and drinks we serve:")
+    allMenus = MenuItem.objects.order_by("title")
+    template = loader.get_template("MenuIngredientMaster/menus.html")
+    context = {
+        "allMenus": allMenus,
+    }
+    return HttpResponse(template.render(context, request))
 
 def itemsInMenu(request, menu_item=None):
     response = "This menu has following ingredients: %s."
