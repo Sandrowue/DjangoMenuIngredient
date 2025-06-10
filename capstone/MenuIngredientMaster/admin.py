1c1e253c987bfc1c666dd20d22125ce486da6456
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2025-06-10 22:33:53 +0300

    kielikorjaus

--- capstone/MenuIngredientMaster/admin.py
+++ capstone/MenuIngredientMaster/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirements
 
 admin.site.register(Ingredient)
 admin.site.register(MenuItem)
 admin.site.register(Purchase)
-admin.site.register(RecipeRequirement)
+admin.site.register(RecipeRequirements)
