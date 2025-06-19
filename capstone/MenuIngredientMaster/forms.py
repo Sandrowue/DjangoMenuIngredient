a160790998596116435aa18f52693af8459b5c69
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2025-06-19 13:51:06 +0300

    cost not in Form

--- capstone/MenuIngredientMaster/forms.py
+++ capstone/MenuIngredientMaster/forms.py
@@ -5,7 +5,7 @@ class RecipeEditForm(forms.Form):
     menu_item = forms.ModelChoiceField(queryset=MenuItem.objects.order_by("title"), label="Menu")
     ingredient = forms.ModelChoiceField(queryset=Ingredient.objects.order_by("name"), label="Ingredient")
     quantity = forms.FloatField(label="Amount")
-    cost = forms.FloatField(label="Price")
+    
 
     
     
