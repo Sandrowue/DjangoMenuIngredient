06704d8d6e13aae7a1328db148daf6a84d0e0713
Sandro.Wüthrich <71585959+Sandrowue@users.noreply.github.com>
2025-07-03 13:50:35 +0300

    no extra path leads to home

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("MenuIngredientMaster.urls")),
     path("MenuIngredientMaster/", include("MenuIngredientMaster.urls")),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
